60a814efd526687f7d34f60e69c1261ac38c2bbd
Andy Dingley <judge40@gmail.com>
2020-01-15 13:50:47 +0000

    Add sentry error tracking to project
    Update package.json to include the @sentry/browser dependency.
    
    Update index.tsx to initialize sentry before the application is
    initialized.

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
+import * as Sentry from '@sentry/browser';
+
+Sentry.init({
+  dsn: "https://abba1f8a43dd4da4a00277b34beaaf59@sentry.io/1882746",
+  environment: process.env.NODE_ENV
+});
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>,
+    document.getElementById("root")
 );
 serviceWorker.register();
