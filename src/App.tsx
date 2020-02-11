f2891273e7a0301bc1f892f1a81e2072fa555c4e
Babul Reddy Yasa <babul3012@gmail.com>
2020-02-11 11:51:22 +0000

    test: jest and enzyme configuration
    TISNEW-3880

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,23 @@
-import React, { Fragment } from 'react';
-import ReactDOM from 'react-dom';
-import './App.scss';
-import Profile from './components/profile/ProfileComponent';
-import BottomNavbar from './components/navigation/BottomNavbarComponent';
-import { Switch, Route, Redirect } from 'react-router-dom';
-import FormR from './components/FormR';
-import LogInComponent from './components/authentication/LogInComponent';
-import Navbar from './components/navigation/NavbarComponent';
-
-if (process.env.NODE_ENV !== 'production') {
-  var axe = require('react-axe');
-  axe(React, ReactDOM, 1000);
-}
+import React, { Fragment } from "react";
+import "./App.scss";
+import Profile from "./components/profile/ProfileComponent";
+import { Switch, Route, Redirect, BrowserRouter } from "react-router-dom";
+import LogInComponent from "./components/authentication/LogInComponent";
+import Navbar from "./components/navigation/NavbarComponent";
 
 const App: React.FC = () => {
   return (
     <Fragment>
       <Navbar />
       <main>
-        <Switch>
-          <Route path="/formr" component={FormR} />
-          <Route exact path="/profile" component={Profile} />
-          <Route path="/login" component={LogInComponent} />
-          <Redirect exact path="/" to="/profile" />
-        </Switch>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/profile" component={Profile} />
+            <Route path="/login" component={LogInComponent} />
+            <Redirect exact path="/" to="/login" />
+          </Switch>
+        </BrowserRouter>
       </main>
-      <footer className="bottom-navigation-menu">
-        <BottomNavbar></BottomNavbar>
-      </footer>
     </Fragment>
   );
 };
