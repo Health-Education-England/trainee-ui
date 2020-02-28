896ace19221202c9d30d1ab00a8671ce839fad2a
John O'Meara <John.OMeara@hee.nhs.uk>
2020-02-28 17:33:04 +0000

    refactor: Add initial Redux boilerplate Set up the store to use thunk and also redux dev tools in chrome to help with debugging.

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import "./index.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import * as Sentry from "@sentry/browser";
 import Amplify from "aws-amplify";
 import config from "./aws-amplify/config";
+
+import store from "./store/store";
 
 Sentry.init({
   dsn: "https://abba1f8a43dd4da4a00277b34beaaf59@sentry.io/1882746",
@@ -22,5 +25,10 @@ Amplify.configure({
   }
 });
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 serviceWorker.register();
---
+++ src/reducers/index.ts
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+//To do - Redux import individual reducers here
+
+export default combineReducers({
+  //to do put reducer names here
+});
