f58e0dab000032b627dd9b8ac038ea267a6471d4
Babul Reddy Yasa <babul3012@gmail.com>
2020-04-15 14:09:23 +0100

    fix: Updated services to use context paths in url
    TISNEW-4247

--- src/services/FormRPartAService.ts
+++ src/services/FormRPartAService.ts
@@ -2,22 +2,18 @@ import { AxiosResponse } from "axios";
 import ApiService from "./apiService";
 import { FormRPartA } from "../models/FormRPartA";
 
-export class FormRPartAService {
-  private apiService: ApiService;
-
+export class FormRPartAService extends ApiService {
   constructor() {
-    this.apiService = new ApiService();
+    super("/forms/api");
   }
 
   async saveTraineeFormRPartA(
     formData: FormRPartA
   ): Promise<AxiosResponse<FormRPartA>> {
-    return this.apiService.post<FormRPartA>("/form/api/formr-parta", formData);
+    return this.post<FormRPartA>("/formr-parta", formData);
   }
 
   async getTraineeFormRPartA(): Promise<AxiosResponse<FormRPartA[]>> {
-    return this.apiService.get<FormRPartA[]>(
-      `/form/api/formr-parta/${this.apiService.traineeTisId}`
-    );
+    return this.get<FormRPartA[]>(`/formr-parta/${this.traineeTisId}`);
   }
 }
