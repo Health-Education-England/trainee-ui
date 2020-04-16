36b6c5e3e0aa51be087bf3c64e64ffe5a72133cb
Babul Reddy Yasa <babul3012@gmail.com>
2020-04-16 12:04:01 +0100

    refactor: Advanced store set up
    * Include redux dev tools in development with compatible browser
    
    TISNEW-4143

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -6,14 +6,17 @@ const initialState = {};
 
 const middleware = [thunk];
 
+const composeEnhancers =
+  process.env.NODE_ENV !== "production" &&
+  typeof window === "object" &&
+  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
-      (window as any).__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeEnhancers(applyMiddleware(...middleware))
 );
 
 export default store;
