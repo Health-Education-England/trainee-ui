ae88a999875856e2b5640b484e6c31350d263540
Babul Reddy Yasa <babul3012@gmail.com>
2020-05-22 11:35:20 +0100

    test: Added unit tests for part-b redux actions and reducers

--- src/redux/__test__/formr-partb-actions.test.ts
+++ src/redux/__test__/formr-partb-actions.test.ts
@@ -2,23 +2,39 @@ import {
   LOAD_FORMR_PARTB_SUCCESS,
   LOAD_FORMR_PARTB_FAILURE,
   LOAD_FORMR_PARTB_LIST_SUCCESS,
-  LOAD_FORMR_PARTB_LIST_FAILURE
+  LOAD_FORMR_PARTB_LIST_FAILURE,
+  LOAD_FORMR_PARTB_INITIAL_VALUES_SUCCESS,
+  LOAD_FORMR_PARTB_INITIAL_VALUES_FAILURE,
+  FORMR_PARTB_NEXT_SECTION,
+  FORMR_PARTB_PREVIOUS_SECTION
 } from "../action_types";
 import thunk from "redux-thunk";
 import configureMockStore from "redux-mock-store";
 import { submittedFormRPartBs } from "../../mock-data/submitted-formr-partb";
 import {
   loadFormRPartB,
-  loadFormRPartBList
+  loadFormRPartBList,
+  loadFormRPartBInitialValues,
+  moveToNextSection,
+  moveToPreviousSection
 } from "../actions/formr-partb-actions";
 import { FormRPartB } from "../../models/FormRPartB";
 import { AxiosResponse } from "axios";
 import { FormsService } from "../../services/FormsService";
+import { TraineeProfileService } from "../../services/TraineeProfileService";
+import { TraineeProfile } from "../../models/TraineeProfile";
+import { mockTraineeProfile } from "../../mock-data/trainee-profile";
+import { ProfileToFormRPartBInitialValues } from "../../models/ProfileToFormRPartBInitialValues";
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 const formsService = new FormsService();
-let store = mockStore({});
+const traineeProfileService = new TraineeProfileService();
+let store = null;
+
+beforeEach(() => {
+  store = mockStore({});
+});
 
 describe("loadFormRPartBList method", () => {
   it("Should dispatch LOAD_FORMR_PARTB_LIST_SUCCESS on successfull api call", () => {
@@ -51,8 +67,6 @@ describe("loadFormRPartBList method", () => {
   });
 
   it("Should dispatch LOAD_FORMR_PARTB_LIST_FAILURE if api call fails", () => {
-    store = mockStore({});
-
     const errorResponse = {
       data: null,
       status: 500,
@@ -80,16 +94,17 @@ describe("loadFormRPartBList method", () => {
 
 describe("loadFormRPartB method", () => {
   it("should dispatch LOAD_FORMR_PARTB_SUCCESS if data is not null", () => {
-    const formrParta = submittedFormRPartBs[0];
+    const formrPartb = submittedFormRPartBs[0];
     const expectedActions = {
       type: LOAD_FORMR_PARTB_SUCCESS,
-      payload: formrParta
+      payload: formrPartb
     };
 
-    return expect(store.dispatch(loadFormRPartB(formrParta))).toEqual(
+    return expect(store.dispatch(loadFormRPartB(formrPartb))).toEqual(
       expectedActions
     );
   });
+
   it("should dispatch LOAD_FORMR_PARTB_FAILURE if data is null", () => {
     const expectedActions = {
       type: LOAD_FORMR_PARTB_FAILURE,
@@ -97,6 +112,88 @@ describe("loadFormRPartB method", () => {
     };
 
     return expect(store.dispatch(loadFormRPartB(null))).toEqual(
+      expectedActions
+    );
+  });
+});
+
+describe("loadFormRPartBInitialValues method", () => {
+  it("should dispatch LOAD_FORMR_PARTB_INITIAL_VALUES_SUCCESS if trainee profile is received", () => {
+    const successResponse: Promise<AxiosResponse<
+      TraineeProfile
+    >> = Promise.resolve({
+      data: mockTraineeProfile,
+      status: 200,
+      statusText: "OK",
+      headers: {},
+      config: {}
+    });
+
+    jest
+      .spyOn(traineeProfileService, "getTraineeProfile")
+      .mockReturnValue(successResponse);
+
+    const expectedAction = {
+      type: LOAD_FORMR_PARTB_INITIAL_VALUES_SUCCESS,
+      payload: ProfileToFormRPartBInitialValues(mockTraineeProfile)
+    };
+
+    return store
+      .dispatch(loadFormRPartBInitialValues(traineeProfileService))
+      .then(() => {
+        expect(store.getActions()).toContainEqual(expectedAction);
+      });
+  });
+
+  it("should dispatch LOAD_FORMR_PARTB_INITIAL_VALUES_FAILURE if trainee profile call fails", () => {
+    const errorResponse = {
+      data: null,
+      status: 500,
+      statusText: "Internal server error",
+      headers: {},
+      config: {}
+    };
+
+    jest
+      .spyOn(traineeProfileService, "getTraineeProfile")
+      .mockReturnValue(Promise.reject(errorResponse));
+
+    const expectedAction = {
+      type: LOAD_FORMR_PARTB_INITIAL_VALUES_FAILURE,
+      payload: errorResponse
+    };
+
+    return store
+      .dispatch(loadFormRPartBInitialValues(traineeProfileService))
+      .then(() => {
+        expect(store.getActions()).toContainEqual(expectedAction);
+      });
+  });
+});
+
+describe("moveToNextSection method", () => {
+  it("should dispatch FORMR_PARTB_NEXT_SECTION", () => {
+    const formrPartb = submittedFormRPartBs[0];
+    const expectedActions = {
+      type: FORMR_PARTB_NEXT_SECTION,
+      payload: formrPartb
+    };
+
+    return expect(store.dispatch(moveToNextSection(formrPartb))).toEqual(
+      expectedActions
+    );
+  });
+});
+
+describe("moveToPreviousSection method", () => {
+  it("should dispatch FORMR_PARTB_PREVIOUS_SECTION", () => {
+    const formrPartb = submittedFormRPartBs[0];
+    const expectedActions = {
+      type: FORMR_PARTB_PREVIOUS_SECTION,
+      payload: formrPartb
+    };
+
+    return expect(store.dispatch(moveToPreviousSection(formrPartb))).toEqual(
       expectedActions
     );
   });
--- src/redux/__test__/formr-partb-reducer.test.ts
+++ src/redux/__test__/formr-partb-reducer.test.ts
@@ -1,16 +1,60 @@
-import { ActionType, FormRPartBViewState, FormRPartBListState } from "../types";
+import {
+  ActionType,
+  FormRPartBViewState,
+  FormRPartBListState,
+  NewFormRPartBState
+} from "../types";
 import {
   LOAD_FORMR_PARTB_SUCCESS,
   LOAD_FORMR_PARTB_FAILURE,
   LOAD_FORMR_PARTB_LIST_SUCCESS,
-  LOAD_FORMR_PARTB_LIST_FAILURE
+  LOAD_FORMR_PARTB_LIST_FAILURE,
+  LOAD_FORMR_PARTB_INITIAL_VALUES_SUCCESS,
+  LOAD_FORMR_PARTB_INITIAL_VALUES_FAILURE,
+  FORMR_PARTB_NEXT_SECTION,
+  FORMR_PARTB_PREVIOUS_SECTION
 } from "../action_types";
 import {
   LoadFormRPartBReducer,
-  LoadFormRPartBListReducer
+  LoadFormRPartBListReducer,
+  LoadNewFormRPartBReducer
 } from "../reducers/formr-partb-reducer";
+import { FormRPartB } from "../../models/FormRPartB";
 
-describe("Load form-r part-a reducer", () => {
+const formrPartB: FormRPartB = {
+  traineeTisId: "123",
+  forename: "Anthony Mara",
+  surname: "Gilliam",
+  gmcNumber: "11111111",
+  localOfficeName: "Health Education England Thames Valley",
+  email: "",
+  programmeSpecialty: "ST6",
+  prevRevalBody: "",
+  currRevalDate: new Date("2021-12-31"),
+  prevRevalDate: new Date("2021-12-31"),
+  dualSpecialty: "",
+  work: [
+    {
+      endDate: new Date("2020-12-31"),
+      site: "Addenbrookes Hospital",
+      siteLocation: undefined,
+      startDate: new Date("2020-01-01"),
+      trainingPost: "Yes",
+      typeOfWork: "In Post ST1 Dermatology"
+    }
+  ],
+  sicknessAbsence: 0,
+  parentalLeave: 0,
+  careerBreaks: 0,
+  paidLeave: 0,
+  unauthorisedLeave: 0,
+  otherLeave: 0,
+  totalLeave: 0,
+  submissionDate: null,
+  lastModifiedDate: null
+};
+
+describe("LoadFormRPartBReducer", () => {
   const initialState: FormRPartBViewState = {
     formData: null
   };
@@ -54,7 +98,7 @@ describe("Load form-r part-a reducer", () => {
   });
 });
 
-describe("Load form-r part-a list reducer", () => {
+describe("LoadFormRPartBListReducer", () => {
   const initialState: FormRPartBListState = {
     submittedForms: []
   };
@@ -73,12 +117,12 @@ describe("Load form-r part-a list reducer", () => {
   it("should return updated state when LOAD_FORMR_PARTB_LIST_SUCCESS action passed", () => {
     const state: FormRPartBListState = {
       ...initialState,
-      submittedForms: []
+      submittedForms: [formrPartB]
     };
 
     const successAction: ActionType = {
       type: LOAD_FORMR_PARTB_LIST_SUCCESS,
-      payload: []
+      payload: [formrPartB]
     };
 
     expect(LoadFormRPartBListReducer(initialState, successAction)).toEqual(
@@ -97,6 +141,113 @@ describe("Load form-r part-a list reducer", () => {
     };
 
     expect(LoadFormRPartBListReducer(initialState, failureAction)).toEqual(
+      state
+    );
+  });
+});
+
+describe("LoadNewFormRPartBReducer", () => {
+  let initialState: NewFormRPartBState = {
+    formData: null,
+    section: 1
+  };
+
+  it("should return initial state when no action passed", () => {
+    const defaultAction: ActionType = {
+      type: "",
+      payload: null
+    };
+
+    expect(LoadNewFormRPartBReducer(undefined, defaultAction)).toEqual(
+      initialState
+    );
+  });
+
+  it("should return updated state when LOAD_FORMR_PARTB_INITIAL_VALUES_SUCCESS action passed", () => {
+    const state: NewFormRPartBState = {
+      ...initialState,
+      formData: formrPartB,
+      section: 1
+    };
+
+    const successAction: ActionType = {
+      type: LOAD_FORMR_PARTB_INITIAL_VALUES_SUCCESS,
+      payload: formrPartB
+    };
+
+    expect(LoadNewFormRPartBReducer(initialState, successAction)).toEqual(
+      state
+    );
+  });
+
+  it("should return updated state when LOAD_FORMR_PARTB_INITIAL_VALUES_FAILURE action passed", () => {
+    const failureAction: ActionType = {
+      type: LOAD_FORMR_PARTB_INITIAL_VALUES_FAILURE,
+      payload: null
+    };
+
+    expect(LoadNewFormRPartBReducer(initialState, failureAction)).toEqual(
+      initialState
+    );
+  });
+
+  it("should add 1 to section when FORMR_PARTB_NEXT_SECTION action passed", () => {
+    const state: NewFormRPartBState = {
+      ...initialState,
+      formData: formrPartB,
+      section: 2
+    };
+
+    const successAction: ActionType = {
+      type: FORMR_PARTB_NEXT_SECTION,
+      payload: formrPartB
+    };
+
+    expect(LoadNewFormRPartBReducer(initialState, successAction)).toEqual(
+      state
+    );
+  });
+
+  it("should reduce section by 1 when FORMR_PARTB_PREVIOUS_SECTION action passed", () => {
+    initialState = {
+      formData: formrPartB,
+      section: 3
+    };
+
+    const state: NewFormRPartBState = {
+      ...initialState,
+      formData: formrPartB,
+      section: 2
+    };
+
+    const successAction: ActionType = {
+      type: FORMR_PARTB_PREVIOUS_SECTION,
+      payload: formrPartB
+    };
+
+    expect(LoadNewFormRPartBReducer(initialState, successAction)).toEqual(
+      state
+    );
+  });
+
+  it("should return section 1 when FORMR_PARTB_PREVIOUS_SECTION action passed", () => {
+    initialState = {
+      formData: formrPartB,
+      section: 1
+    };
+
+    const state: NewFormRPartBState = {
+      ...initialState,
+      formData: formrPartB,
+      section: 1
+    };
+
+    const successAction: ActionType = {
+      type: FORMR_PARTB_PREVIOUS_SECTION,
+      payload: formrPartB
+    };
+
+    expect(LoadNewFormRPartBReducer(initialState, successAction)).toEqual(
       state
     );
   });
