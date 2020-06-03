da0259ab11782ab5994882a7d001403498a5ff43
Babul Reddy Yasa <babul3012@gmail.com>
2020-06-03 13:25:24 +0100

    refactor: Updated boolean utilities function to handle strings

--- src/utilities/BooleanUtilities.ts
+++ src/utilities/BooleanUtilities.ts
@@ -1,5 +1,21 @@
 export class BooleanUtilities {
-  public static ToYesNo(value: boolean | undefined): string {
-    return value ? "Yes" : "No";
+  public static ToYesNo(value: any): string {
+    let boolValue = value && value != null;
+
+    if (typeof value === "string") {
+      boolValue = value?.toLowerCase() === "true";
+    }
+
+    return boolValue ? "Yes" : "No";
+  }
+
+  public static ToBoolean(value: any): boolean {
+    let boolValue = Boolean(value && value != null);
+
+    if (typeof value === "string") {
+      boolValue = value?.toLowerCase() === "true";
+    }
+
+    return boolValue;
   }
 }
