229711b6b516b36339e7d0a40725f495b30491a0
StevenHee <64011863+StevenHee@users.noreply.github.com>
2020-06-19 16:39:07 +0100

    refactor: add form utilities

--- src/utilities/Constants.ts
+++ src/utilities/Constants.ts
@@ -1,3 +1,9 @@
 export const CCT_DECLARATION =
   "I have been appointed to a programme leading to award of CCT";
+
 export const MEDICAL_CURRICULUM = "MEDICAL_CURRICULUM";
+
+export const YES_NO = [
+  { label: "Yes", value: "true" },
+  { label: "No", value: "false" }
+];
