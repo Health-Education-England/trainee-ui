8a8db9dd089f7de7c044f2de280fdb646ede17fd
StevenHee <64011863+StevenHee@users.noreply.github.com>
2020-09-28 13:39:37 +0100

    test: update tests

--- cypress/integration/navbar/navbar.spec.ts
+++ cypress/integration/navbar/navbar.spec.ts
@@ -10,7 +10,6 @@ describe("Desktop/ tablet header", () => {
     it(`should have menu items after successfull sign-in on ${size} screen`, () => {
       cy.viewport(size);
       if (size === mobileView) {
-        cy.get(".nhsuk-header__search-toggle").should("be.visible");
         cy.get(".nhsuk-header__menu-toggle").should("be.visible");
 
         cy.get(".nhsuk-header__menu-toggle").click();
@@ -32,13 +31,5 @@ describe("Desktop/ tablet header", () => {
       cy.contains("Form R-a").should("be.visible").click();
       cy.location("pathname", { timeout: 10000 }).should("include", "/formr-a");
     });
-  });
-
-  it("Should be able to toggle search button to show hide search form on mobbile view", () => {
-    cy.viewport("iphone-6");
-    cy.get(".nhsuk-header__search-toggle > .nhsuk-icon").click();
-    cy.get("#search-field").should("be.visible");
-    cy.get(".nhsuk-search__close > .nhsuk-icon > path").click();
-    cy.get("#search-field").should("not.be.visible");
   });
 });
