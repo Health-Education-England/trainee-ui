24dadb61bac4dbe6864461a6397ec620b31037c4
StevenHee <64011863+StevenHee@users.noreply.github.com>
2020-10-14 20:57:20 +0100

    feat:add and configure cookie message component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CookieConsent from "react-cookie-consent";
 import "./App.scss";
 import Profile from "./components/profile/Profile";
 import { Switch, Route, Redirect, BrowserRouter } from "react-router-dom";
@@ -98,6 +99,35 @@ class App extends React.PureComponent<AppProps, AppState> {
         )}
         ;
         <HEEFooter appVersion={appVersion} />
+        <CookieConsent
+          disableStyles={true}
+          buttonClasses="nhsuk-button nhsuk-button--reverse"
+          buttonWrapperClasses="cookieConsent-button-wrapper"
+          contentClasses="cookieConsent-content"
+          overlayClasses="cookieConsent-overlay"
+          overlay
+          extraCookieOptions={{ SameSite: "Lax" }}
+          location="bottom"
+          buttonText="Accept and close"
+        >
+          <h3>Your consent is required.</h3>
+          <p>
+            We use necessary cookies to make our site work and analytics cookies
+            to help us improve it. In order use this site, you must agree to the
+            storing of such cookies on your device by clicking 'Accept and
+            close'. To learn more about how we use cookies and your data, please
+            see our{" "}
+            <a
+              target="_blank"
+              style={{ color: "#ffffff" }}
+              rel="noopener noreferrer nofollow"
+              href="https://www.hee.nhs.uk/about/privacy-notice"
+            >
+              privacy policy
+            </a>
+            .
+          </p>
+        </CookieConsent>
       </BrowserRouter>
     );
   }
