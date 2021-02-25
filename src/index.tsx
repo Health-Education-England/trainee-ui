419ad23b021c4465614b6d23fcec29180144e655
john12321 <johnomeara72@gmail.com>
2021-02-25 14:06:08 +0000

    chore: Add dynamic env vars
    TO-DO json res needs some work

--- src/index.tsx
+++ src/index.tsx
@@ -10,12 +10,8 @@ import config from "./aws-amplify/config";
 
 import store from "./redux/store/store";
 
-Sentry.init({
-  dsn: "https://abba1f8a43dd4da4a00277b34beaaf59@sentry.io/1882746",
-  environment: process.env.NODE_ENV
-});
-
-Amplify.configure({
+let envName = "development";
+let congitoAuth = {
   Auth: {
     mandatorySignIn: true,
     region: config.cognito.REGION,
@@ -24,7 +20,35 @@ Amplify.configure({
     userPoolWebClientId: config.cognito.APP_CLIENT_ID,
     authenticationFlowType: config.cognito.USER_PASSWORD_AUTH
   }
+};
+
+const fetchEnvdata = () => {
+  fetch("/api/environment")
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Network response error");
+      }
+      return response.json();
+    })
+    .then(envData => {
+      envName = envData["envName"]; // needs checking against json obj
+      congitoAuth = { Auth: JSON.parse(envData["auth"]) }; // needs checking too
+    })
+    .catch(error => {
+      console.error("There is a problem fetching the data: ", error);
+    });
+};
+
+if (process.env.NODE_ENV !== "development") {
+  fetchEnvdata();
+}
+
+Sentry.init({
+  dsn: "https://abba1f8a43dd4da4a00277b34beaaf59@sentry.io/1882746",
+  environment: envName
 });
+
+Amplify.configure(congitoAuth);
 
 ReactDOM.render(
   <Provider store={store}>
