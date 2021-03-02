90794616f389993351de7783f2f8d8d31c08e544
john12321 <johnomeara72@gmail.com>
2021-03-02 18:05:43 +0000

    fix: run lint

--- src/components/forms/formr-part-b/Sections/CovidDeclaration.tsx
+++ src/components/forms/formr-part-b/Sections/CovidDeclaration.tsx
@@ -333,13 +333,17 @@ const CovidDeclaration: FunctionComponent<SectionProps> = (
                     <Label>
                       <p>
                         Please provide details of your Educational Supervisor in
-                        this section.<strong> A PDF copy of this form
-                        will need to be sent to your ES when you submit this
-                        form</strong>. This will give your ES the opportunity to review
-                        the information provided in the self-assessment
-                        declaration, comment and confirm / validate them and
-                        make a recommendation for the ARCP during COVID 19. This
-                        will be completed by the ES in your ePortfolio.
+                        this section.
+                        <strong>
+                          {" "}
+                          A PDF copy of this form will need to be sent to your
+                          ES when you submit this form
+                        </strong>
+                        . This will give your ES the opportunity to review the
+                        information provided in the self-assessment declaration,
+                        comment and confirm / validate them and make a
+                        recommendation for the ARCP during COVID 19. This will
+                        be completed by the ES in your ePortfolio.
                       </p>
                     </Label>
                     <TextInputField
