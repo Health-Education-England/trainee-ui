386507ab37651597982b63db9b7eec02fe383540
john12321 <johnomeara72@gmail.com>
2021-03-02 14:11:53 +0000

    Revert "Merge pull request #333 from Health-Education-England/add/dynamicEnvVars"
    This reverts commit 660c58b7f0e8240c64a78f8684d4a8a5b399fd42, reversing
    changes made to 617d4898e57416435facc068116e6a52fc406b2b.

--- src/index.tsx
+++ src/index.tsx
@@ -10,8 +10,12 @@ import config from "./aws-amplify/config";
 
 import store from "./redux/store/store";
 
-let envName = "development";
-let congitoAuth = {
+Sentry.init({
+  dsn: "https://abba1f8a43dd4da4a00277b34beaaf59@sentry.io/1882746",
+  environment: process.env.NODE_ENV
+});
+
+Amplify.configure({
   Auth: {
     mandatorySignIn: true,
     region: config.cognito.REGION,
@@ -20,35 +24,7 @@ let congitoAuth = {
     userPoolWebClientId: config.cognito.APP_CLIENT_ID,
     authenticationFlowType: config.cognito.USER_PASSWORD_AUTH
   }
-};
-
-const fetchEnvdata = () => {
-  fetch("/api/environment")
-    .then(response => {
-      if (!response.ok) {
-        throw new Error("Network response error");
-      }
-      return response.json();
-    })
-    .then(envData => {
-      envName = envData["envName"]; // needs checking against json obj
-      congitoAuth = { Auth: JSON.parse(envData["auth"]) }; // needs checking too
-    })
-    .catch(error => {
-      console.error("There is a problem fetching the data: ", error);
-    });
-};
-
-if (process.env.NODE_ENV !== "development") {
-  fetchEnvdata();
-}
-
-Sentry.init({
-  dsn: "https://abba1f8a43dd4da4a00277b34beaaf59@sentry.io/1882746",
-  environment: envName
 });
-
-Amplify.configure(congitoAuth);
 
 ReactDOM.render(
   <Provider store={store}>
