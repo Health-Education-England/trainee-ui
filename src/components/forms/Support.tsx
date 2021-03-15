dc5cdfb54a321a24b1a764d0767b847b6aaa95cb
john12321 <johnomeara72@gmail.com>
2021-03-15 11:52:01 +0000

    refactor: Support component

--- src/components/forms/Support.tsx
+++ src/components/forms/Support.tsx
@@ -1,29 +1,14 @@
 import React from "react";
-import { Formik, Form } from "formik";
-import * as yup from "yup";
 import { connect, ConnectedProps } from "react-redux";
-import { Button, Panel } from "nhsuk-react-components";
-import { ToastContainer, toast, ToastOptions } from "react-toastify";
+import { Panel } from "nhsuk-react-components";
 import "react-toastify/dist/ReactToastify.css";
-import {
-  LocalOfficeContact,
-  localOfficeContacts
-} from "../../models/LocalOfficeContacts";
-import {
-  FormParams,
-  EmailTemplateParams
-} from "../../models/FeedbackFormParams";
-import { TraineeProfile } from "../../models/TraineeProfile";
-import { ProgrammeMembership } from "../../models/ProgrammeMembership";
+import { localOfficeContacts } from "../../models/LocalOfficeContacts";
 import { loadTraineeProfile } from "../../redux/actions/trainee-profile-actions";
-
 import { RootState } from "../../redux/reducers";
 import { TraineeProfileService } from "../../services/TraineeProfileService";
-
-import TextInputField from "./TextInputField";
 import Loading from "../common/Loading";
-import ScrollTo from "./ScrollTo";
 import PageTitle from "../common/PageTitle";
+
 const mapStateToProps = (state: RootState) => ({
   traineeProfile: state.profile.traineeProfile,
   isLoaded: state.profile.isLoaded
@@ -35,211 +20,95 @@ const mapDispatchToProps = {
 
 const connector = connect(mapStateToProps, mapDispatchToProps);
 type profileProps = ConnectedProps<typeof connector>;
-
 interface LocalState {
-  sendingEmail: boolean;
-  showForm: boolean;
+  contact: string;
+  dataError: boolean;
+  matchError: boolean;
+  personOwner: string | null | undefined;
 }
-
-//https://www.typescriptlang.org/docs/handbook/advanced-types.html#mapped-types
-export type Partial<T> = {
-  [P in keyof T]?: T[P];
-};
-
-export type CurrentProgrammeMembership =
-  | Partial<ProgrammeMembership>
-  | undefined;
-
-export class UnconnectedSupport extends React.PureComponent<
-  profileProps,
-  LocalState
-> {
+export class Support extends React.PureComponent<profileProps, LocalState> {
   constructor(props: profileProps) {
     super(props);
-    this.state = { sendingEmail: false, showForm: false };
+    this.state = {
+      contact: "",
+      dataError: false,
+      matchError: false,
+      personOwner: ""
+    };
   }
 
-  currentProgramme: CurrentProgrammeMembership = {};
+  fetchTraineeProfileProps = async () =>
+    await this.props.loadTraineeProfile(new TraineeProfileService());
 
-  getLocalOfficeEmail(localOfficeName: string | undefined) {
-    if (localOfficeName) {
-      const currentLocalOffice = localOfficeContacts.find(
-        (localOffice: LocalOfficeContact) => {
-          return localOffice.name === localOfficeName;
-        }
-      );
-      return currentLocalOffice ? currentLocalOffice.email : null;
+  fetchPersonOwner = () => {
+    return this.props.traineeProfile?.personalDetails?.personOwner;
+  };
+
+  findMappedContact(loadedPersonOwner: string | null | undefined) {
+    for (let i = 0; i < localOfficeContacts.length; i++) {
+      if (localOfficeContacts[i].name === loadedPersonOwner) {
+        return localOfficeContacts[i].contact;
+      }
     }
     return null;
   }
 
-  getCurrentProgramme(
-    profile: TraineeProfile | null
-  ): CurrentProgrammeMembership {
-    return profile?.programmeMemberships
-      ? profile.programmeMemberships.find((programme: ProgrammeMembership) => {
-          return programme.status === "CURRENT";
-        })
-      : {};
-  }
-
   async componentDidMount() {
-    await this.props.loadTraineeProfile(new TraineeProfileService());
-
-    this.currentProgramme = this.getCurrentProgramme(this.props.traineeProfile);
-    this.getLocalOfficeEmail(this.currentProgramme?.managingDeanery)
-      ? this.setState({ showForm: true })
-      : this.setState({ showForm: false });
-  }
-
-  mockSendEmail(success: boolean) {
-    return new Promise((resolve, reject) => {
-      window.setTimeout(() => {
-        if (success) {
-          resolve("SUCCESS");
-        } else {
-          reject("FAIL");
-        }
-      }, 1000);
-    });
-  }
-
-  defaultToastOptions: ToastOptions = {
-    position: "bottom-right",
-    autoClose: 5000,
-    hideProgressBar: true,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    className: "customToast"
-  };
-
-  handleSubmit(values: FormParams, resetForm: any) {
-    this.setState({ sendingEmail: true });
-
-    const templateParams: EmailTemplateParams = {
-      surname:
-        this.props.traineeProfile?.personalDetails?.surname ||
-        "No surname associated with trainee profile",
-      forenames:
-        this.props.traineeProfile?.personalDetails?.forenames ||
-        "No forename associated with trainee profile",
-      email:
-        this.props.traineeProfile?.personalDetails?.email ||
-        "No email associated with trainee profile",
-      gmcNumber:
-        this.props.traineeProfile?.personalDetails?.gmcNumber ||
-        "No GMC number associated with trainee profile",
-      programmeName:
-        this.currentProgramme?.programmeName ||
-        "No current programme associated with trainee profile",
-      localOffice:
-        this.currentProgramme?.managingDeanery ||
-        "No current deanery associated with trainee profile",
-      localOfficeEmail: this.getLocalOfficeEmail(
-        this.currentProgramme?.managingDeanery || "noreply@hee.nhs.uk"
-      ),
-      message: values.message
-    };
-
-    this.mockSendEmail(true)
-      .then(() => {
-        toast.success("Message sent", {
-          ...this.defaultToastOptions,
-          className: "customToast successToast"
-        });
-        resetForm({});
-        console.log(templateParams);
-      })
-      .catch(() => {
-        toast.error("Message failed to send", {
-          ...this.defaultToastOptions,
-          className: "autoClose: false, customToast failToast"
-        });
-      })
-
-      .finally(() => {
-        this.setState({ sendingEmail: false });
-      });
+    await this.fetchTraineeProfileProps();
+    const fetchedPersonOwner = this.fetchPersonOwner();
+    if (!fetchedPersonOwner) {
+      this.setState({ dataError: true });
+      return;
+    }
+    this.setState({ personOwner: fetchedPersonOwner });
+    const mappedContact = this.findMappedContact(fetchedPersonOwner);
+    if (mappedContact) {
+      this.setState({ contact: mappedContact });
+    } else this.setState({ matchError: true });
   }
 
   render() {
     const { traineeProfile, isLoaded } = this.props;
+    const { contact, dataError, matchError, personOwner } = this.state;
 
     if (!isLoaded) {
       return <Loading />;
+    }
+    if (dataError) {
+      return (
+        <div data-jest="dataErrorPanel">
+          <p>
+            {`Sorry but your Local Office could not be found. We need this to route your query to the right place. For further assistance, We suggest you raise this with your supervisor.`}
+          </p>
+        </div>
+      );
+    }
+    if (matchError) {
+      return (
+        <div data-jest="matchErrorPanel">
+          <p>
+            {`Sorry but your Local Office name ${personOwner} cannot be matched with a support contact email address. We suggest you raise this with your supervisor.`}
+          </p>
+        </div>
+      );
     } else {
       return (
         traineeProfile && (
-          <div id="feedback">
-            <PageTitle title="Support" />
-            <ScrollTo />
-            <ToastContainer />
-            <h1>Support</h1>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint ad
-              ipsum, magni architecto ratione in cumque nihil eveniet numquam.
-              Molestias eos quibusdam libero accusantium officia consequuntur.
-              Architecto ducimus laborum similique?
-            </p>
-
-            {this.state.showForm ? (
-              <div data-jest="FormPanel">
-                <Panel label="Submit your question">
-                  <Formik
-                    initialValues={{ message: "" }}
-                    validationSchema={yup.object({
-                      message: yup.string().required("Message is required!")
-                    })}
-                    onSubmit={(values, { resetForm }) => {
-                      this.handleSubmit(values, resetForm);
-                    }}
-                  >
-                    {({ values, errors }) => (
-                      <Form>
-                        <TextInputField
-                          rows={10}
-                          label="Enter your query"
-                          name="message"
-                          hidelabel={true}
-                          disabled={this.state.sendingEmail}
-                        />
-                        <div className="nhsuk-grid-row">
-                          <div className="nhsuk-grid-column-full">
-                            <div
-                              style={{
-                                display: "flex",
-                                justifyContent: "flex-end"
-                              }}
-                            >
-                              <Button
-                                disabled={this.state.sendingEmail}
-                                data-cy="BtnSendEmail"
-                                data-jest="btnSendEmail"
-                              >
-                                {this.state.sendingEmail ? (
-                                  <span>Sending...</span>
-                                ) : (
-                                  <span>Send</span>
-                                )}
-                              </Button>
-                            </div>
-                          </div>
-                        </div>{" "}
-                      </Form>
-                    )}
-                  </Formik>
-                </Panel>
-              </div>
-            ) : (
-              <div data-jest="GetHelpPanel">
-                <Panel label="How to get help">
+          <>
+            <div>
+              <PageTitle title="Support" />
+              <h1>Support</h1>
+              <p>
+                If you have a query relating to completing the Form R - which
+                includes queries about the information we currrently hold for
+                you - then please follow the instructions below.
+              </p>
+            </div>
+            {contact === "PGMDE" ? (
+              <div data-jest="PGMDESupportPanel">
+                <Panel label="Support via the PGMDE Support Portal">
                   <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Amet maiores voluptatum voluptas totam nostrum illum omnis
-                    harum aliquid, distinctio id doloribus quos dignissimos
-                    iure, magni odit quam. Odit, repudiandae asperiores.
+                    {`According to our records, your Local Office is ${personOwner}. Please click on the link below to send your query to the team via the PGMDE Support Portal.`}
                   </p>
                   <p>
                     <a href="https://lasepgmdesupport.hee.nhs.uk/support/tickets/new?form_7=true">
@@ -248,11 +117,23 @@ export class UnconnectedSupport extends React.PureComponent<
                   </p>
                 </Panel>
               </div>
+            ) : (
+              <div data-jest="loSupportPanel">
+                <Panel label="Support from your Local Office">
+                  <p>
+                    {`According to our records, your Local Office name is ${personOwner}. Please email them your query by either clicking on the link below (which should open your email client) or copying the email
+                    address and pasting it into your email client.`}
+                  </p>
+                  <p>
+                    <a href={`mailto:${contact}`}>{contact}</a>
+                  </p>
+                </Panel>
+              </div>
             )}
-          </div>
+          </>
         )
       );
     }
   }
 }
-export default connect(mapStateToProps, mapDispatchToProps)(UnconnectedSupport);
+export default connect(mapStateToProps, mapDispatchToProps)(Support);
