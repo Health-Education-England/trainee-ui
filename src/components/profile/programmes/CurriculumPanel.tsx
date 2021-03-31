6f94ad552a22650a37f4bcf27629097d09a01879
john12321 <johnomeara72@gmail.com>
2021-03-31 18:20:27 +0100

    chore: bump version

--- src/components/profile/programmes/CurriculumPanel.tsx
+++ src/components/profile/programmes/CurriculumPanel.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Curriculum } from "../../../models/ProgrammeMembership";
 import { DateUtilities } from "../../../utilities/DateUtilities";
+import styles from "./CurriculumPanel.module.scss";
 
 interface ICurriculumPanelProps {
   curriculum: Curriculum;
@@ -10,9 +11,9 @@ export const CurriculumPanel = (props: ICurriculumPanelProps) => {
   const curricData = props.curriculum;
 
   return (
-    <div>
+    <div className={styles.cItems}>
       <div>
-        {curricData.curriculumName}
+        {curricData.curriculumName}{" "}
         {curricData.curriculumSubType
           ? `(${curricData.curriculumSubType})`
           : null}
@@ -21,7 +22,6 @@ export const CurriculumPanel = (props: ICurriculumPanelProps) => {
         {DateUtilities.ToLocalDate(curricData.curriculumStartDate)} -{" "}
         {DateUtilities.ToLocalDate(curricData.curriculumEndDate)}
       </div>
-      <hr className="nhsuk-u-padding-0 nhsuk-u-margin-bottom-1 nhsuk-u-margin-top-1" />
     </div>
   );
 };
