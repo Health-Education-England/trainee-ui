f12cd7db96a74c9c7264029f105ca57263b8478e
john12321 <johnomeara72@gmail.com>
2021-06-24 17:39:36 +0100

    refactor: reduce cognitive complexity (sonarcloud)

--- src/utilities/ReferenceDataUtilities.ts
+++ src/utilities/ReferenceDataUtilities.ts
@@ -15,4 +15,17 @@ export class ReferenceDataUtilities {
   ) {
     return [...tisId].some(id => this.getIdFromLabel(refData, label) === id);
   }
+
+  public static isMatchedItem(
+    refData: KeyValue[],
+    formDataProp: string
+  ): boolean {
+    return refData.some(element => element.label === formDataProp);
+  }
+
+  public static checkDataProp(refData: KeyValue[], formDataProp: string) {
+    if (!this.isMatchedItem(refData, formDataProp)) {
+      return "";
+    } else return formDataProp;
+  }
 }
